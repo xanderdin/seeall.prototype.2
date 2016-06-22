1a82ef4fff7d621cca6450b086c73bd21d3c0b29
Alexander Pravdin <aledin@mail.ru>
2016-06-22 10:23:37 +0300

    Added spinner on login submit.

--- imports/ui/pages/auth-page.js
+++ imports/ui/pages/auth-page.js
@@ -1,1 +1,13 @@
+import { Template } from 'meteor/templating';
+import { AccountsTemplates } from 'meteor/useraccounts:core';
+
+import '/imports/ui/components/loading.js';
+
 import './auth-page.html';
+
+
+Template.Auth_page.helpers({
+  atDisabled() {
+    return AccountsTemplates.disabled();
+  }
+});
