450d04132fbb5ca09f9bc897b08faa883db65435
Alexander Pravdin <aledin@mail.ru>
2016-06-24 12:07:06 +0300

    Nothing special

--- imports/startup/client/at-config.js
+++ imports/startup/client/at-config.js
@@ -4,6 +4,11 @@ import { TAPi18n } from 'meteor/tap:i18n';
 import '/imports/ui/pages/auth-page.js';
 
 
+// See:
+// https://github.com/meteor-useraccounts/core
+// https://github.com/meteor-useraccounts/core/blob/master/Guide.md
+// https://github.com/meteor-useraccounts/flow-routing
+
 AccountsTemplates.configure({
   defaultLayoutType: 'blaze',
   // showForgotPasswordLink: true,
