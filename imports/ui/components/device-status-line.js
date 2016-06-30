ca13d6f1d2a7d77c552367baca24cbee6f192267
Alexander Pravdin <aledin@mail.ru>
2016-06-30 15:43:33 +0300

    Removed unneeded console.log() line.

--- imports/ui/components/device-status-line.js
+++ imports/ui/components/device-status-line.js
@@ -6,7 +6,6 @@ import './device-status-line.html';
 Template.DeviceStatusLine.helpers({
   isLowMoney() {
     const moneyThreshold = 10; // FIXME: Should set it from user settings
-    console.log(this.device.simBalance);
     if (this.device.simBalance < moneyThreshold) {
       return true;
     }
